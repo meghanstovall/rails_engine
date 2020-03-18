cb24c12675369d0b213d1bb1a1b344f41d5378cf
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-18 14:46:36 -0600

    Write test to make sure an item can be created

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -23,13 +23,14 @@ describe 'Items API' do
     expect(item["attributes"]["id"]).to eq(id)
   end
 
-  # it "can create a new item" do
-  #   item_params = {name: "Saw", description: "I want to play a game", unit_price: 25}
-  #
-  #   post "/api/v1/items", params: {item: item_params}
-  #   item = Item.last
-  #
-  #   expect(response).to be_successful
-  #   expect(item.name).to eq(item_params[:name])
-  # end
+  it "can create a new item" do
+    merchant_id = create(:merchant).id
+    item_params = {name: "Saw", description: "I want to play a game", unit_price: 25, merchant_id: merchant_id}
+
+    post "/api/v1/items", params: {item: item_params}
+    item = Item.last
+
+    expect(response).to be_successful
+    expect(item.name).to eq(item_params[:name])
+  end
 end
