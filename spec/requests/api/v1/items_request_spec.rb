f0f75215028284f7a64fea2433f5447f7952bc48
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-18 15:13:23 -0600

    Add test to item and merchant request specs to check they can be updated

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -33,4 +33,17 @@ describe 'Items API' do
     expect(response).to be_successful
     expect(item.name).to eq(item_params[:name])
   end
+
+  it "can update an existing item" do
+    id = create(:item).id
+    previous_name = Item.last.name
+    item_params = { name: "Sledge" }
+
+    put "/api/v1/items/#{id}", params: {item: item_params}
+    item = Item.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(item.name).to_not eq(previous_name)
+    expect(item.name).to eq("Sledge")
+  end
 end
