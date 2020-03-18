1f35ca6410059729edc52688fcf8c39965facd7a
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-17 20:55:53 -0600

    Add create action for item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -7,4 +7,13 @@ class Api::V1::ItemsController < ApplicationController
   def show
     render json: ItemSerializer.new(Item.find(params[:id]))
   end
+
+  def create
+    render json: Item.create(item_params)
+  end
+
+  private
+    def item_params
+      params.require(:item).permit(:name, :description, :unit_price)
+    end
 end
