c92526d1cbf722184f2ee7a2b89c2e2491cc6f34
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-18 14:36:55 -0600

    Add test and model method to convert the invoice_items unit prices to dollars

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,4 +2,8 @@ class InvoiceItem < ApplicationRecord
   validates_presence_of :quantity, :unit_price
   belongs_to :item
   belongs_to :invoice
+
+  def convert_price
+    unit_price / 100.to_f
+  end
 end
