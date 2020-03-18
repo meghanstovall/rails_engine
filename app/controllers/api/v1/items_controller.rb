266fa1555108a06e5f524163a1c7774455bd63fe
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-18 14:47:45 -0600

    Add ItemSerializer to create method as well as merchant_id to item_params method

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -9,11 +9,11 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   def create
-    render json: Item.create(item_params)
+    render json: ItemSerializer.new(Item.create(item_params))
   end
 
   private
     def item_params
-      params.require(:item).permit(:name, :description, :unit_price)
+      params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
     end
 end
