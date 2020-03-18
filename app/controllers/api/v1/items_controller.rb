c8fb23392728198d306af973b5474d21fd6028f5
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-18 15:31:27 -0600

    Add destroy action to both controllers

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -16,6 +16,10 @@ class Api::V1::ItemsController < ApplicationController
     render json: ItemSerializer.new(Item.update(item_params))
   end
 
+  def destroy
+    render json: Item.delete(params[:id])
+  end
+
   private
     def item_params
       params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
