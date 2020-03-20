5b6b2b8a587307671f509e343f0c4803b957bf3d
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-19 21:07:09 -0600

    add namespace for items and merchants for the find route

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,18 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
 
+      namespace :items do
+        get "/find", to: "search#show"
+      end
+
       resources :items, except: [:new, :edit] do
         scope module: 'items' do
           get "/merchant", to: "merchants#show"
         end
+      end
+
+      namespace :merchants do
+        get "/find", to: "search#show"
       end
 
       resources :merchants, except: [:new, :edit] do
