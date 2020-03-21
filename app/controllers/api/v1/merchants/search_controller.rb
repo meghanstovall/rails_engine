86fe5b3c8530cfd50556e10403b4f808e4f5606d
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-20 21:05:37 -0600

    Edit index and show actions for finders. Used like to be able to do partial searches

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,7 +1,18 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def show
+    # render json: MerchantSerializer.new(Merchant.where(request.query_parameters))
+
     if search_params[:name]
-      render json: MerchantSerializer.new(Merchant.where("lower(name) = ?", search_params[:name].downcase))
+      attribute = search_params.keys.first
+      render json: MerchantSerializer.new(Merchant.where("lower(#{attribute}) = ?", search_params[attribute.to_sym].downcase))
+    else
+      render json: MerchantSerializer.new(Merchant.where(search_params))
+    end
+  end
+
+  def index
+    if search_params[:name]
+      render json: MerchantSerializer.new(Merchant.where('name ILIKE ? ', "%#{search_params[:name]}%"))
     else
       render json: MerchantSerializer.new(Merchant.where(search_params))
     end
