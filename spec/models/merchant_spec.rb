9deeb77f39211b1c78cb0f571a6e172a910e8429
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-21 09:20:16 -0600

    Add two model test for both merchants and items for a single finder and multi finder model method

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -10,4 +10,23 @@ RSpec.describe Merchant, type: :model do
     it {should have_many :items}
     it {should have_many :invoices}
   end
+
+  describe 'methods' do
+    it "#single_search" do
+      merchant1 = Merchant.create!(name: "Tom")
+      merchant2 = Merchant.create!(name: "Tommy")
+      search_params = {name: "Tom"}
+
+      expect(Merchant.single_search(search_params).first.class).to eq(Merchant)
+    end
+
+    it "#multi_search" do
+      merchant1 = Merchant.create!(name: "Tom")
+      merchant2 = Merchant.create!(name: "Tommy")
+      merchant3 = Merchant.create!(name: "Marry")
+      search_params = {name: "Tom"}
+
+      expect(Merchant.multi_search(search_params).count).to eq(2)
+    end
+  end
 end
