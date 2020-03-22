4be742eba8602859bd1baef1289d92e39fe233ce
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-21 20:38:49 -0600

    change single search model method to account for partial matches

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,7 +8,7 @@ class Merchant < ApplicationRecord
   def self.single_search(search_params)
     if search_params[:name]
       attribute = search_params.keys.first
-      Merchant.where("lower(#{attribute}) = ?", search_params[attribute.to_sym].downcase).limit(1)
+      Merchant.where('name ILIKE ? ', "%#{search_params[:name]}%").limit(1)
     else
       Merchant.where(search_params).limit(1)
     end
@@ -26,5 +26,18 @@ class Merchant < ApplicationRecord
     joins(:transactions, :invoice_items).where(transactions: {result: "success"})
       .select('merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue')
       .group(:id).order('revenue DESC').limit(limit_num.to_i)
+  end
+
+  def self.most_items(limit_num)
+    require "pry"; binding.pry
+    joins(:transactions, :invoice_items).where(transactions: {result: "success"})
+      .select('merchants.*')
+  end
+
+  def self.merchant_revenue(merchant_id)
+    joins(:transactions, :invoice_items)
+      .where("transactions.result = 'success' AND merchants.id = #{merchant_id}")
+      .select('merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue')
+      .group(:id).first.revenue
   end
 end
