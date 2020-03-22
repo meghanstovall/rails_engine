1a768bd19e17abf62abb8a1db75eda4b0d95d2fe
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-22 17:27:24 -0600

    delete total revenue from merchants controller and moved to new controller, uncomment most_items method

--- app/controllers/api/v1/revenue_controller.rb
+++ app/controllers/api/v1/revenue_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::RevenueController < ApplicationController
 
   def total_revenue
-    start_date = params[:start].to_date
-    end_date = params[:end].to_date
+    start_date = params[:start].to_time
+    end_date = params[:end].to_time
     total_revenue = Merchant.total_date_revenue(start_date, end_date)
     render json: {data: {id: nil, attributes: {revenue: total_revenue}}}
   end
