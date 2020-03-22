a97fcdc205adcb8e198b748c67b0a8f87b5002cc
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-21 20:37:47 -0600

    Test that we can return a merchants revenue

--- spec/requests/api/v1/merchants/merchant_business_spec.rb
+++ spec/requests/api/v1/merchants/merchant_business_spec.rb
@@ -12,9 +12,12 @@ describe 'Merchants API' do
       @invoice2 = Invoice.create!(status: "shipped", customer: @customer, merchant: @merchant2)
       @invoice3 = Invoice.create!(status: "shipped", customer: @customer, merchant: @merchant3)
 
-      @item1 = Item.create!(name: "fishing pole", description: "good for begginers", unit_price: 20, merchant: @merchant1)
-      @item2 = Item.create!(name: "mac book pro", description: "good for begginer coders", unit_price: 1000, merchant: @merchant2)
+      @item1 = Item.create!(name: "fishing pole", description: "good for beginners", unit_price: 20, merchant: @merchant1)
+      @item2 = Item.create!(name: "mac book pro", description: "good for beginner coders", unit_price: 1000, merchant: @merchant2)
       @item3 = Item.create!(name: "dog bowl", description: "for small dogs", unit_price: 5, merchant: @merchant3)
+      @item4 = Item.create!(name: "fly fishing pole", description: "good for advanced", unit_price: 80, merchant: @merchant1)
+      @item5 = Item.create!(name: "rain coat", description: "red and all sizes", unit_price: 15, merchant: @merchant1)
+      @item6 = Item.create!(name: "t-shirt", description: "good for everyone", unit_price: 5, merchant: @merchant2)
 
       @pole_willms = InvoiceItem.create!(quantity: 1, unit_price: 20, item: @item1, invoice: @invoice1)
       @mac_turing = InvoiceItem.create!(quantity: 10, unit_price: 1000, item: @item2, invoice: @invoice2)
@@ -34,6 +37,27 @@ describe 'Merchants API' do
       expect(merchants.count).to eq(2)
       expect(merchants[0][:attributes][:name]).to eq(@merchant2.name)
       expect(merchants[-1][:attributes][:name]).to eq(@merchant3.name)
+    end
+
+    # it "can return variable number of merchants that have sold the most items" do
+    #   get "/api/v1/merchants/most_items?quantity=2"
+    #
+    #   merchants = JSON.parse(response.body, symbolize_names: true)[:data]
+    #
+    #   expect(response).to be_successful
+    #   expect(merchants.count).to eq(2)
+    #   expect(merchants[0][:attributes][:name]).to eq(@merchant1.name)
+    #   expect(merchants[-1][:attributes][:name]).to eq(@merchant2.name)
+    # end
+
+    it "can return a merchants revenue" do
+      get "/api/v1/merchants/#{@merchant1.id}/revenue"
+
+      data = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(response).to be_successful
+      expect(data[:id]).to eq(@merchant1.id)
+      expect(data[:attributes][:revenue]).to eq(20.0)
     end
   end
 end
