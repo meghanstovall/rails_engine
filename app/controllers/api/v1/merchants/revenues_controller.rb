1d6d9df72776b552f74f2273c31c7f778d7af9c0
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-21 20:39:24 -0600

    add show action and most_items method to merchants revenues controller

--- app/controllers/api/v1/merchants/revenues_controller.rb
+++ app/controllers/api/v1/merchants/revenues_controller.rb
@@ -3,4 +3,13 @@ class Api::V1::Merchants::RevenuesController < ApplicationController
   def index
     render json: MerchantSerializer.new(Merchant.most_revenue(params[:quantity]))
   end
+
+  def show
+    revenue_num = Merchant.merchant_revenue(params[:id].to_i)
+    render json: {data: {id: params[:id].to_i, attributes: {revenue: revenue_num}}}
+  end
+
+  def most_items
+    render json: MerchantSerializer.new(Merchant.most_items(params[:quantity]))
+  end
 end
