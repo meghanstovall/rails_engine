892097238a08b08ad7c78f0c60dec0d25518cda6
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-23 08:06:50 -0600

    change the most_items controller method to an index method

--- app/controllers/api/v1/merchants/revenues_controller.rb
+++ app/controllers/api/v1/merchants/revenues_controller.rb
@@ -4,10 +4,6 @@ class Api::V1::Merchants::RevenuesController < ApplicationController
     render json: MerchantSerializer.new(Merchant.most_revenue(params[:quantity]))
   end
 
-  def most_items
-    render json: MerchantSerializer.new(Merchant.most_items(params[:quantity]))
-  end
-
   def show
     revenue_num = Merchant.merchant_revenue(params[:id].to_i)
     render json: {data: {id: params[:id].to_i, attributes: {revenue: revenue_num}}}
